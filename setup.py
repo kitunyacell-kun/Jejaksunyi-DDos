9b93671e72532663fe785fab9a007f639db8892a
kitunyacell-kun <kitunyacell@gmail.com>
2026-02-05 04:25:20 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,11 @@ print("""[0] pip\n[1] pip3\nWhich one do you use?""")
 
 c = input(">>>: ")
 if c == "0":
-    os.system("pip install scapy")
-    os.system("pip install datetime")
     os.system("pip install colorama")
-    
-    os.system("pip install aiohttp")
+    os.system("pip install randomstring")
+    os.system("pip install colorama")
 elif c == "1":
-    os.system("pip3 install scapy")
-    os.system("pip3 install datetime")
+    os.system("pip3 install colorama")
+    os.system("pip3 install randomstring")
     os.system("pip3 install colorama")
 print("Done.")
